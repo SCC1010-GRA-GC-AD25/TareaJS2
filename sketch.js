74dffb7841076a9d2ee54674cc47a062eaae3e18
JCJC2104 <jcarlosdiazbeltran1@gmail.com>
2025-11-20 02:48:53 +0000

    listo

--- sketch.js
+++ sketch.js
@@ -8,7 +8,29 @@ function setup() {
 }
 
 function draw() {
-  // put drawing code here
-  background(255)
-  rect(100,100,200,200)
+  background(220);
+    fill(255, 220, 180);
+  ellipse(200, 120, 80, 80);
+
+  fill(0);
+  ellipse(185, 115, 8, 8);
+  ellipse(215, 115, 8, 8);
+  arc(200, 135, 30, 20, 0, PI);
+
+  fill(255, 100, 150);
+  triangle(200, 160, 160, 250, 240, 250);
+
+  stroke(255, 220, 180);
+  strokeWeight(6);
+  line(160, 180, 120, 200); 
+  line(240, 180, 280, 200); 
+
+  stroke(255, 220, 180);
+  strokeWeight(8);
+  line(185, 250, 185, 300);
+  line(215, 250, 215, 300);
+
+  stroke(0);
+  line(175, 300, 195, 300);
+  line(205, 300, 225, 300);
 }
